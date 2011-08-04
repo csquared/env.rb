60ab12e3c734d722ac377444f9c9b2847406e2b2
Chris Continanza <christopher.continanza@gmail.com>
2011-08-03 21:30:23 -0700

    move lib code out of test and into lib/env.rb

--- lib/env.rb
+++ lib/env.rb
@@ -1,5 +1,42 @@
 require "env/version"
 
+class EnvironmentError < StandardError
+end
+
 module Env
-  # Your code goes here...
+  @@dependencies = []
+  @@env          = {}
+
+  class << self
+    def dependencies
+      @@dependencies
+    end
+
+    def [](key)
+      return @@env[key] if dependencies.include? key
+      raise EnvironmentError, "#{key} is not a declared depency, add it to your Envfile"
+      @@env[key]
+    end
+
+    def export(key, value = nil)
+      @@dependencies << key
+      @@env[key] = value
+    end
+
+    def enforce
+      class << ENV
+        alias_method :get, :[]  
+        alias_method :set, :[]= 
+
+        def [](key)
+          Env[key]
+        end
+
+        def []=(key, value)
+          puts "muhaha #{key}"
+          set(key)
+        end
+      end
+    end
+  end
 end
--- spec/enforce_spec.rb
+++ spec/enforce_spec.rb
@@ -1,44 +1,4 @@
-module Env
-  @@dependencies = []
-  @@env          = {}
-
-  class << self
-    def dependencies
-      @@dependencies
-    end
-
-    def [](key)
-      return @@env[key] if dependencies.include? key
-      raise EnvironmentError, "#{key} is not a declared depency, add it to your Envfile"
-      @@env[key]
-    end
-
-    def export(key, value = nil)
-      @@dependencies << key
-      @@env[key] = value
-    end
-
-    def enforce
-      class << ENV
-        alias_method :get, :[]  
-        alias_method :set, :[]= 
-
-        def [](key)
-          Env[key]
-        end
-
-        def []=(key, value)
-          puts "muhaha #{key}"
-          set(key)
-        end
-      end
-    end
-  end
-end
-
-class EnvironmentError < StandardError
-end
-
+require_relative '../lib/env'
 
 describe Env, "::enforce" do
   before { Env.enforce }
